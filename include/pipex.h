babfb66a69e285c007699481dcba3661f8ac06f6
Dominik Kaiser <dkaiser@3-F-2.42heilbronn.de>
2024-05-02 16:47:43 +0200

    Put env util functions in separate file

--- include/pipex.h
+++ include/pipex.h
@@ -6,14 +6,27 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 14:04:28 by dkaiser           #+#    #+#             */
-/*   Updated: 2024/04/29 16:38:38 by dkaiser          ###   ########.fr       */
+/*   Updated: 2024/05/02 16:22:31 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include "../libft/libft.h"
+# include "libft.h"
 # include <unistd.h>
+
+typedef struct s_pxdata
+{
+	int		in_fd;
+	int		out_fd;
+	char	**cmds;
+}			t_pxdata;
+
+char		**get_split_path(char *envp[]);
+char		*get_pwd(char *envp[]);
+char		*find_in_path(char *cmd, char **path);
+char		*get_cmd_path(char *cmd, char **path, char *pwd);
+t_pxdata	*get_pxdata(int argc, char *argv[], char *envp[]);
 
 #endif // PIPEX_H
--- src/input_handling.c
+++ src/input_handling.c
@@ -6,7 +6,7 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 12:13:23 by dkaiser           #+#    #+#             */
-/*   Updated: 2024/05/02 15:36:43 by dkaiser          ###   ########.fr       */
+/*   Updated: 2024/05/02 16:42:01 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,67 +15,6 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-static char	**get_split_path(char *envp[])
-{
-	char	*path;
-
-	while (!ft_strnstr(*envp, "PATH=", 5))
-		envp++;
-	if (!*envp)
-		return (NULL);
-	path = *envp + 5;
-	return (ft_split(path, ':'));
-}
-
-static char	*get_pwd(char *envp[])
-{
-	while (!ft_strnstr(*envp, "PWD=", 4))
-		envp++;
-	if (!*envp)
-		return (NULL);
-	return (*envp + 4);
-}
-
-static char	*find_in_path(char *cmd, char **path)
-{
-	char	*cur_path;
-	char	*cmd_path;
-
-	cmd_path = NULL;
-	while (*path)
-	{
-		if (cmd_path)
-			free(cmd_path);
-		cur_path = ft_strjoin(*path, "/");
-		// TODO: Free on fail
-		cmd_path = ft_strjoin(cur_path, cmd);
-		free(cur_path);
-		if (access(cmd_path, X_OK) != -1)
-			return (cur_path);
-		path++;
-	}
-	return (NULL);
-}
-
-static char	*get_cmd_path(char *cmd, char **path, char *pwd)
-{
-	char	*cur_dir;
-
-	if (cmd[0] == '/')
-		return (cmd); // TODO: Maybe use duplicate instead, so there will be no problem on free()
-	else if (strchr(cmd, '/'))
-	{
-		cur_dir = ft_strjoin(pwd, "/");
-		// TODO: Free on fail
-		// TODO: Maybe check if executable, else there might be a problem...
-		return (ft_strjoin(cur_dir, cmd));
-	}
-	else
-    {
-        return (find_in_path(cmd, path));
-    }
-}
 
 static char	**get_cmds(int argc, char *argv[], char *envp[])
 {
